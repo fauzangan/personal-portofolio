7906b6cced8edb910489e6274e764c5965845f55
Fauzan Zaman <63907675+fauzangan@users.noreply.github.com>
2024-02-17 14:33:26 +0700

    Update banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,7 @@ import { isVisible } from "@testing-library/user-event/dist/utils";
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const toRotate = ["Web Developer", "Web Designer", "Terminator"];
+  const toRotate = ["Web Developer", "Back-end Engineer", "Full Stack Engineer"];
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const period = 2000;
